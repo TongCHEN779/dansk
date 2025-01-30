3283dc9afdfeb44e3df93ef5baa571e133cb0ce0
Tong Chen <victorchen779@gmail.com>
2025-01-30 13:18:05 +0100

    updates

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -12,56 +12,108 @@ var $hlinks = $('#site-nav .hidden-links');
 
 var breaks = [];
 
-function updateNav() {
+// function updateNav() {
 
+//   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+//   // The visible list is overflowing the nav
+//   if($vlinks.width() > availableSpace) {
+
+//     // Record the width of the list
+//     breaks.push($vlinks.width());
+
+//     // Move item to the hidden list
+//     $vlinks.children('*:not(.masthead__menu-item--lg)').last().prependTo($hlinks);
+
+//     // Show the dropdown btn
+//     if($btn.hasClass('hidden')) {
+//       $btn.removeClass('hidden');
+//     }
+
+//   // The visible list is not overflowing
+//   } else {
+
+//     // There is space for another item in the nav
+//     if(availableSpace > breaks[breaks.length-1]) {
+
+//       // Move the item to the visible list
+//       $hlinks.children().first().appendTo($vlinks);
+//       breaks.pop();
+//     }
+
+//     // Hide the dropdown btn if hidden list is empty
+//     if(breaks.length < 1) {
+//       $btn.addClass('hidden');
+//       $hlinks.addClass('hidden');
+//     }
+//   }
+
+//   // Keep counter updated
+//   $btn.attr("count", breaks.length);
+
+//   // Recur if the visible list is still overflowing the nav
+//   if($vlinks.width() > availableSpace) {
+//     updateNav();
+//   }
+
+// }
+
+// // Window listeners
+
+// $(window).resize(function() {
+//   updateNav();
+// });
+
+// $btn.on('click', function() {
+//   $hlinks.toggleClass('hidden');
+//   $(this).toggleClass('close');
+// });
+
+// updateNav();
+
+function updateNav() {
   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
 
+  // 获取不包含子菜单的宽度
+  var visibleWidth = 0;
+  $vlinks.children(':not(.submenu)').each(function() {
+    visibleWidth += $(this).outerWidth(true);  // 计算单个菜单项的宽度
+  });
+
   // The visible list is overflowing the nav
-  if($vlinks.width() > availableSpace) {
+  if (visibleWidth > availableSpace) {
+    breaks.push(visibleWidth);
 
-    // Record the width of the list
-    breaks.push($vlinks.width());
+    // 仅移动 **主菜单项**（排除有子菜单的项）
+    $vlinks.children('*:not(.masthead__menu-item--lg, .has-submenu)').last().prependTo($hlinks);
 
-    // Move item to the hidden list
-    $vlinks.children('*:not(.masthead__menu-item--lg)').last().prependTo($hlinks);
-
-    // Show the dropdown btn
-    if($btn.hasClass('hidden')) {
+    if ($btn.hasClass('hidden')) {
       $btn.removeClass('hidden');
     }
 
-  // The visible list is not overflowing
   } else {
-
-    // There is space for another item in the nav
-    if(availableSpace > breaks[breaks.length-1]) {
-
-      // Move the item to the visible list
+    if (availableSpace > breaks[breaks.length - 1]) {
       $hlinks.children().first().appendTo($vlinks);
       breaks.pop();
     }
 
-    // Hide the dropdown btn if hidden list is empty
-    if(breaks.length < 1) {
+    if (breaks.length < 1) {
       $btn.addClass('hidden');
       $hlinks.addClass('hidden');
     }
   }
 
-  // Keep counter updated
   $btn.attr("count", breaks.length);
 
-  // Recur if the visible list is still overflowing the nav
-  if($vlinks.width() > availableSpace) {
+  // 避免无限递归
+  if (visibleWidth > availableSpace && breaks.length < $vlinks.children().length) {
     updateNav();
   }
-
 }
 
-// Window listeners
-
+// 监听窗口变化
 $(window).resize(function() {
-  updateNav();
+  setTimeout(updateNav, 100);  // 延迟执行，避免抖动
 });
 
 $btn.on('click', function() {
@@ -69,4 +121,5 @@ $btn.on('click', function() {
   $(this).toggleClass('close');
 });
 
-updateNav();
+// 初始加载时调用
+setTimeout(updateNav, 100);
